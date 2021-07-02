725b7af1a7fb8a3f161147c526a8f5c169fe1bdd
Zernez <fernando.pannullo@yahoo.it>
2021-07-02 10:53:50 +0200

    Assignment Week 2 Angular

--- Angular/conFusion/src/app/about/about.component.ts
+++ Angular/conFusion/src/app/about/about.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Leader } from '../shared/leader';
+import { LeaderService } from '../services/leader.service';
+
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
+
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+
+  leader: Leader [];
+
+  constructor(private leaderservice: LeaderService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit() {
+    
+    this.leader = this.leaderservice.getLeaders();
   }
 
 }
