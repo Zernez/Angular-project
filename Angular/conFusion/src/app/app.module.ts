b08179f4757840fcb00bd5359bd00d8f163017ac
Zernez <fernando.pannullo@yahoo.it>
2021-07-03 17:11:48 +0200

    Promise Part 2

--- Angular/conFusion/src/app/app.module.ts
+++ Angular/conFusion/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { AppComponent } from './app.component';
 
@@ -64,7 +65,8 @@ import { MatDialogModule } from '@angular/material/dialog';
     MatCheckboxModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatProgressSpinnerModule
   ],
 
   providers: [DishService, PromotionService, LeaderService],
