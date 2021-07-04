618247c0633f31902ebe54f21f6154ea17b858a1
Zernez <fernando.pannullo@yahoo.it>
2021-07-04 11:10:02 +0200

    RxJs Part 1

--- Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -20,9 +20,9 @@ export class DishdetailComponent implements OnInit {
     private location: Location) { }
 
   ngOnInit() {
+    
     const id = this.route.snapshot.params['id'];
-    this.dishservice.getDish(id)
-      .then(dish => this.dish = dish);
+    this.dishservice.getDish(id).subscribe(dish => this.dish = dish);
   }
 
   goBack(): void {
