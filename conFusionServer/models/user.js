597ba508ae77b4a9641f772294de521dd18af51c
Zernez <fernando.pannullo@yahoo.it>
2021-07-28 22:44:36 +0200

    Mongoose Population

--- conFusionServer/models/user.js
+++ conFusionServer/models/user.js
@@ -3,16 +3,15 @@ var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
+    firstname: {
+      type: String,
+        default: ''
     },
-    password: {
-        type: String,
-        required: true
+    lastname: {
+      type: String,
+        default: ''
     },
-    admin: {
+    admin:   {
         type: Boolean,
         default: false
     }
